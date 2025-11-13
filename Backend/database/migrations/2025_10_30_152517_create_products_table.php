2ede524786862e59523632714f00ffbd3489b749
ianhjweng <ianhjweng@outlook.com>
2025-11-13 13:55:04 +0000

    Improved table functionality

--- Backend/database/migrations/2025_10_30_152517_create_products_table.php
+++ Backend/database/migrations/2025_10_30_152517_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('productCategory', 100)->index('idx_category');
             $table->decimal('productPrice', 10)->index('idx_price');
             $table->text('productDescription');
+            $table->unsignedInteger('productQty')->default(0);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrentOnUpdate()->useCurrent();
         });
