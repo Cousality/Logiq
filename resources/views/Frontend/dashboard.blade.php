708ec2dfa7f127a657d5bb6933d0676babbae908
Cousality <165505726+Cousality@users.noreply.github.com>
2025-11-18 15:36:35 +0000

    added temporary titles for features

--- resources/views/Frontend/dashboard.blade.php
+++ resources/views/Frontend/dashboard.blade.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
-
-
+<body>
+    @include('Frontend.components.navbar')
+    <h1> hello x </h1>
+    <h1> change password </h1>
+    <h1> change addresses </h1>
+    <h1> change name </h1>
+    <h1> order history </h1>
+    
+</body>
+@include('Frontend.components.footer')
 
 </html>
