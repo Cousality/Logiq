80acd4d722a23ea3d655e4bb31f852291c9b5562
Cousality <165505726+Cousality@users.noreply.github.com>
2025-11-19 19:11:20 +0000

    Create product file and controller

--- resources/views/Frontend/components/product_card.blade.php
+++ resources/views/Frontend/components/product_card.blade.php
@@ -1,18 +1,20 @@
 @foreach ($products as $product)
-    <div class="product-card" data-category="{{ $product->productCategory }}"
-        data-difficulty="{{ $product->productDifficulty }}">
-        <div class="product-image">
-            @if ($product->productImage)
-                <img src="{{ asset('storage/' . $product->productImage) }}" alt="{{ $product->productName }}">
-            @else
-                Product Image
-            @endif
+    <a href="{{ route('product.show', $product->productID) }}" class="product-card-link">
+        <div class="product-card" data-category="{{ $product->productCategory }}"
+            data-difficulty="{{ $product->productDifficulty }}">
+            <div class="product-image">
+                @if ($product->productImage)
+                    <img src="{{ asset('storage/' . $product->productImage) }}" alt="{{ $product->productName }}">
+                @else
+                    Product Image
+                @endif
+            </div>
+            <div class="product-info">
+                <div class="product-name">{{ $product->productName }}</div>
+                <div class="product-price">£{{ number_format($product->productPrice, 2) }}</div>
+                <button class="add-to-basket" data-product-id="{{ $product->productID }}">Add to Basket</button>
+                <button class="add-to-wishlist" data-product-id="{{ $product->productID }}">Add to Wishlist</button>
+            </div>
         </div>
-        <div class="product-info">
-            <div class="product-name">{{ $product->productName }}</div>
-            <div class="product-price">£{{ number_format($product->productPrice, 2) }}</div>
-            <button class="add-to-basket" data-product-id="{{ $product->productID }}">Add to Basket</button>
-            <button class="add-to-wishlist" data-product-id="{{ $product->productID }}">Add to Wishlist</button>
-        </div>
-    </div>
+    </a>
 @endforeach
