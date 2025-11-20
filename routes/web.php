0384866ce9fa7660beb8424a52689e3e72d900cd
Cousality <165505726+Cousality@users.noreply.github.com>
2025-11-20 13:48:07 +0000

    removed duplicates in web.php returned FAQ route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BasketController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\WishlistController;
-use App\Http\Controllers\BasketController;
-use App\Http\Controllers\CheckoutController;
 
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
-
 
 use Illuminate\Support\Facades\Route;
 
@@ -45,6 +44,12 @@ Route::post('/wishlist/add', [WishlistController::class, 'add'])->name('wishlist
 Route::post('/wishlist/remove', [WishlistController::class, 'remove'])->name('wishlist.remove');
 Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
 
+//Basket Controller
+Route::get('/basket', [BasketController::class, 'index'])->name('basket.index');
+Route::post('/basket/add', [BasketController::class, 'add'])->name('basket.add');
+Route::put('/basket/{item}', [BasketController::class, 'update'])->name('basket.update');
+Route::delete('/basket/{item}', [BasketController::class, 'remove'])->name('basket.remove');
+
 Route::get('/product/{productSlug}', [ProductController::class, 'index'])->name('product.index');
 
 Route::get('/your_orders', [OrderController::class, 'index'])->name('dashboard.orders');
@@ -60,9 +65,6 @@ Route::get('/forgot-password', function () {
 Route::post('/send-reset-link', function () {
     return back()->with('message', 'Reset link sent (demo)');
 });
-Route::get('/basket', function () {
-    return view('Frontend.basket');
-});
 
 Route::get('/About_us', function () {
     return view('Frontend.about_us');
@@ -74,6 +76,10 @@ Route::get('/privacy_policy', function () {
 
 Route::get('/TermsConditions', function () {
     return view('Frontend.TermsConditions');
+});
+
+Route::get('/FAQs', function () {
+    return view('Frontend.FAQs');
 });
 
 //Dashboard Routes
@@ -92,24 +98,3 @@ Route::get('/login_security', function () {
 Route::get('/return_policy', function () {
     return view('Frontend.return_policy');
 })->name('return_policy');
-
-
-
-Route::get ('/return_policy', function () {
-    return view('Frontend.return_policy');
-});
-
-
-
-Route::get('/basket', [BasketController::class, 'index'])->name('basket.index');
-
-Route::post('/basket/add', [BasketController::class, 'add'])->name('basket.add');
-
-Route::put('/basket/{item}', [BasketController::class, 'update'])->name('basket.update');
-
-Route::delete('/basket/{item}', [BasketController::class, 'remove'])->name('basket.remove');
-
-
-
-
-
