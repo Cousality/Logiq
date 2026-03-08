f7f65e3eb2b0883c38cf325b2c783abee6df4c9c
Iman-elber <eiman.06@outlook.com>
2026-03-08 15:09:45 +0000

    dark mode header change

--- resources/views/Frontend/dashboard/stock_analysis.blade.php
+++ resources/views/Frontend/dashboard/stock_analysis.blade.php
@@ -12,7 +12,7 @@
             padding: 4rem 5%;
             background: linear-gradient(135deg,
                     var(--bg-primary) 60%,
-                    var(--red-pastel-1) 60%);
+                    var(--red-pastel-2) 60%);
             border-bottom: 2px solid var(--text);
         }
 
